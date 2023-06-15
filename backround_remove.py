4a41e74e079538fec8b8712cd014d7ac2bf1992f
Mauricio Bustamante <mbustamante@berkeley.edu>
2023-06-15 15:23:20 -0700

    Removes background and thresholds for selecting a ROI in which to look for good points

--- backround_remove.py
+++ backround_remove.py
@@ -43,19 +43,55 @@ plt.imshow(colorConvert(frame_median))
 # %%
 
 frame_avg = np.average(frames, axis = 0).astype(dtype = np.uint8)
+gray_frame_avg = cv.cvtColor(frame_avg, cv.COLOR_BGR2GRAY)
 plt.imshow(colorConvert(frame_avg))
 # %%
 gray_frame_median = cv.cvtColor(frame_median, cv.COLOR_BGR2GRAY)
 plt.imshow(colorConvert(gray_frame_median))
      
 # %%
+frame_sample = frames[0]
 
-
-gray_frame_sample = cv.cvtColor(frame_sample, cv2.COLOR_BGR2GRAY)
+gray_frame_sample = cv.cvtColor(frame_sample, cv.COLOR_BGR2GRAY)
 plt.imshow(colorConvert(gray_frame_sample))
 # %%
-bg_removed_frame = cv.absdiff(gray_frame_sample, gray_frame_median)
+bg_removed_frame = cv.absdiff(gray_frame_sample, gray_frame_avg)
 plt.imshow(colorConvert(bg_removed_frame))
-     
+# %%
 
+
+ret,th = cv.threshold(bg_removed_frame,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+ret2,th2 = cv.threshold(bg_removed_frame,ret*0.9,255, cv.THRESH_BINARY)
+kernel = np.ones((5,5),np.uint8)
+dilated = cv.dilate(th2, kernel, iterations=5)
+dilated = cv.morphologyEx(dilated, cv.MORPH_OPEN, kernel)
+plt.imshow(colorConvert(th2))
+plt.imshow(colorConvert(dilated))
+# %%
+bg_rmoved_color = cv.absdiff(frame_sample, frame_avg)
+plt.imshow(colorConvert(bg_rmoved_color*10))
+
+# %% threschold 
+# %%
+# cap = cv.VideoCapture(video_file)
+# backSub = cv.createBackgroundSubtractorMOG2()
+# while True:
+#     ret, frame = cap.read()
+#     if frame is None:
+#         break
+    
+#     fgMask = backSub.apply(frame)
+    
+    
+#     cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+#     cv.putText(frame, str(cap.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+#                cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+    
+    
+#     cv.imshow('Frame', frame)
+#     cv.imshow('FG Mask', fgMask)
+    
+#     keyboard = cv.waitKey(30)
+#     if keyboard == 'q' or keyboard == 27:
+#         break
 # %%
